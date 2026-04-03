c63d3e5307a8ec70dbe656242b1cead14aa40328
Quang <vinhquangcontroller1@gmail.com>
2026-04-04 01:27:21 +0700

    Fix rendering syntax in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import TestAI from './TestAI';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TestAI />
   </StrictMode>,
